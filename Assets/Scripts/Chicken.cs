4897ddd1dea8d1e7c5f899988311b47252bb21af
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2024-03-11 21:41:51 +1100

    Car Collision

--- Assets/Scripts/Chicken.cs
+++ Assets/Scripts/Chicken.cs
@@ -6,10 +6,11 @@ using UnityEngine.UIElements;
 public class Chicken : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private BoxCollider2D bc2d;
+    [SerializeField] private CapsuleCollider2D cc2d;
     [SerializeField] private float moveSpeed;
     bool stunned = false;
     int eggs = 0;
+    int collisionStrength = 5;
 
     private void FixedUpdate()
     {
@@ -19,15 +20,22 @@ public class Chicken : MonoBehaviour
         }
     }
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Car"))
         {
-            if (!stunned)
-            { 
-                stunned = true;
-                rb.velocity = new Vector2(gameObject.transform.position.x - collision.transform.position.x, gameObject.transform.position.y - collision.transform.position.y);
-            }
+            stunned = true;
+            rb.velocity = new Vector2((gameObject.transform.position.x - collision.transform.position.x)* collisionStrength, (gameObject.transform.position.y - collision.transform.position.y)* collisionStrength);
+            StartCoroutine(carCrash());
         }
+    }
+
+    IEnumerator carCrash()
+    {
+        yield return new WaitForSeconds(0.5f);
+        rb.velocity = new Vector2(0,0);
+        yield return new WaitForSeconds(1.5f);
+        stunned = false;
     }
 }
